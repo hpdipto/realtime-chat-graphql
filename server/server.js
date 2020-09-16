bfc73964e22e3f63a94abe2b796834ec3adc72e7
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-16 12:55:02 +0600

    Full project without module federation

---
+++ server/server.js
@@ -0,0 +1,60 @@
+const { GraphQLServer, PubSub } = require("graphql-yoga");
+
+const messages = [];
+
+const typeDefs = `
+	type Messages {
+		id: ID!
+		user: String!
+		content: String!
+	}
+
+	type Query {
+		messages: [Messages!]
+	}
+
+	type Mutation {
+		postMessage(user: String!, content: String!): ID!
+	}
+
+	type Subscription {
+		messages: [Messages!]
+	}
+`;
+
+const subscribers = [];
+const onMessagesUpdates = (fn) => subscribers.push(fn);
+
+const resolvers = {
+	Query: {
+		messages: () => messages,
+	},
+	Mutation: {
+		postMessage: (parent, { user, content }) => {
+			const id = messages.length;
+			messages.push({
+				id,
+				user,
+				content,
+			});
+			subscribers.forEach((fn) => fn());
+			return id;
+		},
+	},
+	Subscription: {
+		messages: {
+			subscribe: (parent, args, { pubsub }) => {
+				const channel = Math.random().toString(36).slice(2, 15);
+				onMessagesUpdates(() => pubsub.publish(channel, { messages }));
+				setTimeout(() => pubsub.publish(channel, { messages }), 0);
+				return pubsub.asyncIterator(channel);
+			},
+		},
+	},
+};
+
+const pubsub = new PubSub();
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
+server.start(({ port }) => {
+	console.log(`Server started on ${port}`);
+});
